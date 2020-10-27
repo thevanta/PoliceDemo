3b2cb722fc1d73ea5fa60d2e229a0bb450adfc94
cody-rowland <68308819+cody-rowland@users.noreply.github.com>
2020-10-27 14:17:22 -0700

    Changed scene naming

---
+++ PoliceDemo/Assets/AnimationRecorder.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationRecorder : MonoBehaviour
+{
+
+    public float timeToRecord;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartRecording(timeToRecord);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void StartRecording(float timeToRecord)
+    {
+        StartCoroutine(RecordMovement(timeToRecord));
+    }
+
+    IEnumerator RecordMovement(float timeLimit)
+    {
+        yield return new WaitForSeconds(timeLimit);
+    }
+}
