7ca7723b5478d5ed871593c4c72119bb333d87f2
cody-rowland <68308819+cody-rowland@users.noreply.github.com>
2020-11-12 17:26:03 -0800

    Added car interior and police lights

---
+++ PoliceDemo/Assets/_Project/Scripts/CopLight.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+
+
+
+public class CopLight : MonoBehaviour {
+
+	public float Offset;
+	public bool flickering = false;
+	public bool flicker = false;
+	public float delayFlicker = 0;
+	public float speedFlicker = 0.4f;
+	public float rotationsPerMinute = 50f;
+	public float rotationSpeedModifier = 1f;
+	public bool rotationEnabled = false;
+	public Light myLight;
+
+	private void Awake()
+	{
+		myLight = GetComponent<Light>();
+	}
+
+	void Start()
+	{
+		transform.Rotate(0, Offset, 0);
+		StartCoroutine(Flashing());
+	}
+
+	private void OnEnable()
+	{
+		myLight.enabled = true;
+	}
+	
+	private void OnDisable()
+	{
+		myLight.enabled = false;
+	}
+
+	void Update ()
+	{
+		if (rotationEnabled)
+		{
+			transform.Rotate(0, 6.0f * rotationsPerMinute * rotationSpeedModifier * Time.deltaTime, 0);
+		}
+	}
+	
+	
+	IEnumerator Flashing ()
+	{
+		if (flickering == true) {
+			yield return new WaitForSeconds (delayFlicker);
+			while (true) 
+			{
+				yield return new WaitForSeconds (speedFlicker);
+				myLight.intensity = 0;
+				yield return new WaitForSeconds (speedFlicker);
+				myLight.intensity = 3;
+				Debug.Log ("Flicker");
+			}
+		}
+	}
+}
+
+
+
+
+
---
+++ PoliceDemo/Assets/_Project/Scripts/LightManager.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+
+public class LightManager : MonoBehaviour {
+
+	public CopLight[] LightList;
+	public Light[] floodLights;
+	public float rotationsPerMinute = 50.0f;
+	public float rotationsPerMinute2 = 70.0f;
+	private float downTime, upTime, pressTime = 0;
+	private float countDown = 0.2f;
+	private bool horn = false;
+	private float mode = 0;
+	private int sirenMode = 0;
+	private Light[] lights;
+	public GameObject lightParent;
+	private AudioSource Sound;
+	public AudioClip Siren1;
+	public AudioClip Siren2;
+	public AudioClip Horn;
+
+
+	void Awake()
+	{
+		Sound = GetComponent<AudioSource> ();
+	}
+	
+
+	// Use this for initialization
+	void Start () {
+		LightList = GetComponentsInChildren<CopLight>();
+		Debug.Log(LightList.Length);
+		lights = lightParent.GetComponentsInChildren<Light>();
+		foreach (Light light in lights)
+		{
+			light.enabled = false; //turn off the light at start of game
+		}
+	}
+
+	[Button]
+	public void EnableLights()
+	{
+		foreach (CopLight light in LightList)
+		{
+			light.enabled = true; //turn off the light at start of game
+			light.rotationEnabled = true;
+		}
+
+		foreach (Light floodLight in floodLights)
+		{
+			floodLight.enabled = true;
+		}
+	}
+	
+	[Button]
+	public void DisableLights()
+	{
+		foreach (CopLight light in LightList)
+		{
+			light.enabled = false; //turn off the light at start of game
+			light.rotationEnabled = false;
+		}
+		
+		foreach (Light floodLight in floodLights)
+		{
+			floodLight.enabled = false;
+		}
+	}
+}
